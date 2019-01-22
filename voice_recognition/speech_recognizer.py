4056a13e915c4e6a8e4e4fdc7c766501259b8a5e
a.redkovsky <a.redkovsky@itransition.com>
2019-01-22 17:00:49 +0300

    delay calibrated

--- voice_recognition/speech_recognizer.py
+++ voice_recognition/speech_recognizer.py
@@ -1,20 +1,22 @@
 import speech_recognition as sr
 import file_manager as fm
 
-FILE_NAME = '/home/andrey/Documents/Projects/WordParasite/logs/parsed_speech.txt'
+FILE_NAME = '/home/ITRANSITION.CORP/a.redkovsky/Documents/Projects/VoiceRecognition/logs/parsed_speech.txt'
 
 # obtain audio from the microphone
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
     while True:
+        r.adjust_for_ambient_noise(source)
         print("Say something!")
-        audio = r.listen(source)
+        audio = r.listen(source, phrase_time_limit=5)
 
         # recognize speech using Google Speech Recognition
         try:
-            fm.write_to_file(FILE_NAME, r.recognize_google(audio, language='ru') + '\n')
+            parsed_text = r.recognize_google(audio, language='ru')
+            print(parsed_text)
+            fm.write_to_file(FILE_NAME, parsed_text + '\n')
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
